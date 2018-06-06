03a67119db4040467c97a87e000268f56d8bf807
yeyuguo <346243440@qq.com>
2018-06-06 21:59:15 +0800

    modified:   src/output/screenshot.js

--- src/output/screenshot.js
+++ src/output/screenshot.js
@@ -2,32 +2,32 @@
  * 在无头浏览器中将一个网页截图保存为图片
  */
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 const saveScreenshot = async (url, path) => {
+  // 启动浏览器
+  const browser = await puppeteer.launch()
+  // 打开页面
+  const page = await browser.newPage()
+  // 设置浏览器视窗
+  page.setViewport({
+    width: 1376,
+    height: 768
+  })
+  // 地址栏输入网页地址
+  await page.goto(url)
+  // 截图: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagescreenshotoptions
+  await page.screenshot({
+    path
+  })
+  // 关闭浏览器
+  await browser.close()
+}
 
-    // 启动浏览器
-    const browser = await puppeteer.launch();
-    // 打开页面
-    const page = await browser.newPage();
-    // 设置浏览器视窗
-    page.setViewport({
-        width: 1376,
-        height: 768,
-    });
-    // 地址栏输入网页地址
-    await page.goto(url);
-    // 截图: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagescreenshotoptions
-    await page.screenshot({ path });
-    // 关闭浏览器
-    await browser.close();
-};
-
-module.exports = saveScreenshot;
-
+module.exports = saveScreenshot
 
 if (require.main === module) {
-    // for test
-    saveScreenshot('http://127.0.0.1:8080/src/output/echart.html', './echart.png');
-    // saveScreenshot('http://google.com', './google.png');
+  // for test
+  saveScreenshot('http://127.0.0.1:8080/src/output/echart.html', './echart.png');
+  // saveScreenshot('http://google.com', './google.png');
 }
